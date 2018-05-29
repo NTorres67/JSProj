aaafce16f54d582b70e53f627a7fb7043282af52
NTorres67 <NTorres1967@gmail.com>
2018-05-29 17:51:58 -0400

    Statements.HTML: statements, semicolons, white space, line length/line break, code blocks

---
+++ JSProj/JavaScript.js
@@ -0,0 +1,12 @@
+﻿function myFunction3() {
+    document.getElementById("demo9").innerHTML = "Here we go again!";
+}
+
+function myFunction4() {
+    document.getElementById("demo10").innerHTML = "Here we go again and again!";
+}
+
+//function myFunction5() {
+//    document.getElementById("demo16").innerHTML = "Hello Dolly!";
+//    document.getElementById("demo17").innerHTML = "How are you?";
+//}
